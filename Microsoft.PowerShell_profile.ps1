d60d26ff074448083c27f6d1b8250b5533441af0
jkoshita <131411389+jkoshita@users.noreply.github.com>
2024-09-18 02:35:46 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,5 +16,5 @@ New-Alias -Name macos.lan -Value C:\Users\jkoshita\.ssh\login\macos.ps1
 New-Alias -Name raspberrypi-1.lan -Value C:\User\jkoshita\.ssh\login\raspberrypi-1.ps1
 # ssh -i /Users/jkoshita/.ssh/raspberrypi-1.lan -L 5900:localhost:5900 jkoshita@raspberrypi-1.lan
 
-New-Alias -Name raspberrypi-1.lan -Value C:\User\jkoshita\.ssh\login\raspberrypi-2.ps1
+New-Alias -Name raspberrypi-2.lan -Value C:\User\jkoshita\.ssh\login\raspberrypi-2.ps1
 # ssh -i /Users/jkoshita/.ssh/raspberrypi-2.lan -L 5901:localhost:5900 jkoshita@raspberrypi-2.lan
