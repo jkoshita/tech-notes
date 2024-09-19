2de92b5915a9d707a5fe7d9943d191fdf7296073
jkoshita <131411389+jkoshita@users.noreply.github.com>
2024-09-19 16:25:50 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -17,7 +17,12 @@ New-Alias -Name raspberrypi.lan -Value C:\Users\jkoshita\.ssh\login\raspberrypi.
 # ssh -i /Users/jkoshita/.ssh/raspberrypi.lan -L 5900:localhost:5900 jkoshita@raspberrypi.lan
 
 New-Alias -Name debian-vm-1.lan -Value C:\Users\jkoshita\.ssh\login\debian-vm-1.ps1
-# ssh -i /Users/jkoshita/.ssh/debian-vm-1.lan -L 5901:localhost:5901 jkoshita@debian.lan
+# param($IPAddress)
+# if ($null -eq $IPAddress ) {
+#     Write-Host "`nRun As Administrator:`nGet-VM -Name debian | Select-Object -ExpandProperty networkadapters | Select-Object -ExpandProperty ipaddresses | Select-Object -First 1`n`ndebian-vm-2.lan -IPAddress <output>`n"
+#     exit
+# }
+# ssh -i /Users/jkoshita/.ssh/debian-vm-1.lan -L 5901:localhost:5901 jkoshita@${IPAddress}
 
 New-Alias -Name debian-vm-2.lan -Value C:\Users\jkoshita\.ssh\login\debian-vm-2.ps1
 # param($IPAddress)
