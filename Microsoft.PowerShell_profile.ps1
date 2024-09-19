648063c826f45dcde29ebf14eed6c0fe89429f61
jkoshita <131411389+jkoshita@users.noreply.github.com>
2024-09-19 12:40:12 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -25,4 +25,4 @@ New-Alias -Name debian-vm-2.lan -Value C:\Users\jkoshita\.ssh\login\debian-vm-2.
 #     Write-Host "`nRun As Administrator:`nGet-VM -Name debian | Select-Object -ExpandProperty networkadapters | Select-Object -ExpandProperty ipaddresses | Select-Object -First 1`n`ndebian-vm-2.lan -IPAddress <output>`n"
 #     exit
 # }
-# ssh -i /Users/jkoshita/.ssh/debian-vm-2.lan -L 5901:localhost:5900 jkoshita@${IPAddress}
+# ssh -i /Users/jkoshita/.ssh/debian-vm-2.lan -L 5901:localhost:5901 jkoshita@${IPAddress}
