555cc52bb314654fe875eef5293a3593cba03a6f
jkoshita <131411389+jkoshita@users.noreply.github.com>
2024-09-24 07:05:01 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,10 @@ New-Alias -Name linksys.lan -Value C:\Users\jkoshita\.ssh\login\linksys.ps1
 # ssh -i /Users/jkoshita/.ssh/linksys.lan -L 18080:localhost:80 root@linksys.lan
 
 New-Alias -Name windows-1.lan -Value C:\Users\jkoshita\.ssh\login\windows-1.ps1
-# ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 34000:localhost:4000 -D 8080 jkoshita@jkoshita.duckdns.org
+# ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 3389:localhost:3389 -D 8080 jkoshita@jkoshita.dyndns.org
 
 New-Alias -Name windows-1.lan_macos -Value C:\Users\jkoshita\.ssh\login\windows-1.lan_macos.ps1
-# ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 24000:localhost:24000 jkoshita@jkoshita.duckdns.org
+# ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 4000:localhost:4000 jkoshita@jkoshita.dyndns.org
 
 New-Alias -Name windows-2.lan -Value C:\Users\jkoshita\.ssh\login\windows-2.ps1
 # ssh -i /Users/jkoshita/.ssh/windows-2.lan -L 14000:localhost:4000 jkoshita@windows-2.lan
