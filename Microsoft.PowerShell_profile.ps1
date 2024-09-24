33b0aa7c5bb4ef6b4d28c7f5cae57a4a009ae16c
jkoshita <131411389+jkoshita@users.noreply.github.com>
2024-09-24 07:08:30 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,7 +8,7 @@ New-Alias -Name windows-1.lan_macos -Value C:\Users\jkoshita\.ssh\login\windows-
 # ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 4000:localhost:4000 jkoshita@jkoshita.dyndns.org
 
 New-Alias -Name windows-2.lan -Value C:\Users\jkoshita\.ssh\login\windows-2.ps1
-# ssh -i /Users/jkoshita/.ssh/windows-2.lan -L 14000:localhost:4000 jkoshita@windows-2.lan
+# ssh -i /Users/jkoshita/.ssh/windows-2.lan -L 3389:localhost:3389 jkoshita@windows-2.lan
 
 New-Alias -Name macos.lan -Value C:\Users\jkoshita\.ssh\login\macos.ps1
 # ssh -i /Users/jkoshita/.ssh/macos.lan -L 4000:localhost:4000 jkoshita@macos.lan
