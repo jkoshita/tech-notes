41e97b7090e9bc862237c44e99ece26277517f06
jkoshita <131411389+jkoshita@users.noreply.github.com>
2024-10-19 12:31:49 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,11 @@
 New-Alias -Name linksys.lan -Value C:\Users\jkoshita\.ssh\login\linksys.ps1
-# ssh -i /Users/jkoshita/.ssh/linksys.lan -L 18080:localhost:80 root@linksys.lan
+# ssh -i /Users/jkoshita/.ssh/linksys.lan -L 18080:localhost:80 root@192.168.1.1
+
+New-Alias -Name linksys-relay1.lan -Value C:\Users\jkoshita\.ssh\login\linksys-relay1.ps1
+# ssh -i /Users/jkoshita/.ssh/linksys.lan -L 8080:localhost:80 root@192.168.1.100
+
+New-Alias -Name linksys-relay2.lan -Value C:\Users\jkoshita\.ssh\login\linksys-relay2.ps1
+# ssh -i /Users/jkoshita/.ssh/linksys.lan -L 8080:localhost:80 root@192.168.1.101
 
 New-Alias -Name windows-1.lan -Value C:\Users\jkoshita\.ssh\login\windows-1.ps1
 # ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 40001:localhost:4000 -D 8080 jkoshita@jkoshita.dyndns.org
@@ -8,25 +14,10 @@ New-Alias -Name windows-1.lan_macos -Value C:\Users\jkoshita\.ssh\login\windows-
 # ssh -i /Users/jkoshita/.ssh/windows-1.lan -L 40002:localhost:40002 jkoshita@jkoshita.dyndns.org
 
 New-Alias -Name windows-2.lan -Value C:\Users\jkoshita\.ssh\login\windows-2.ps1
-# ssh -i /Users/jkoshita/.ssh/windows-2.lan -L 40003:localhost:4000 jkoshita@windows-2.lan
+# ssh -i /Users/jkoshita/.ssh/windows-2.lan -L 40003:localhost:4000 jkoshita@192.168.1.102
 
 New-Alias -Name macos.lan -Value C:\Users\jkoshita\.ssh\login\macos.ps1
-# ssh -i /Users/jkoshita/.ssh/macos.lan -L 40002:localhost:4000 jkoshita@macos.lan
-
-New-Alias -Name proxmox.lan -Value C:\Users\jkoshita\.ssh\login\proxmox.ps1
-# ssh -i /Users/jkoshita/.ssh/proxmox.lan root@192.168.1.151
-
-New-Alias -Name debian-fs.lan -Value C:\Users\jkoshita\.ssh\login\debian-fs.ps1
-# ssh -i /Users/jkoshita/.ssh/debian-fs.lan jkoshita@debian-fs.lan
-
-New-Alias -Name windows-server.lan -Value C:\Users\jkoshita\.ssh\login\windows-server.ps1
-# ssh -i /Users/jkoshita/.ssh/windows-server.lan administrator@windows-server.lan
+# ssh -i /Users/jkoshita/.ssh/macos.lan -L 40002:localhost:4000 jkoshita@192.168.1.103
 
 New-Alias -Name raspberrypi.lan -Value C:\Users\jkoshita\.ssh\login\raspberrypi.ps1
-# ssh -i /Users/jkoshita/.ssh/raspberrypi.lan -L 5900:localhost:5900 jkoshita@raspberrypi.lan
-
-New-Alias -Name linksys-relay1.lan -Value C:\Users\jkoshita\.ssh\login\linksys-relay1.ps1
-# ssh -i /Users/jkoshita/.ssh/linksys.lan -L 8080:localhost:80 root@192.168.1.100
-
-New-Alias -Name linksys-relay2.lan -Value C:\Users\jkoshita\.ssh\login\linksys-relay2.ps1
-# ssh -i /Users/jkoshita/.ssh/linksys.lan -L 8080:localhost:80 root@192.168.1.101
+# ssh -i /Users/jkoshita/.ssh/raspberrypi.lan -L 5900:localhost:5900 jkoshita@192.168.1.104
